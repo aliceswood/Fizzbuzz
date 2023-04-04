d5357e1a1dd89fcdfec418acac9f6628fc06a9de
Will Davies <william.johnston.davies@gmail.com>
2023-04-04 13:49:15 +0000

    passing tests for multiples of 5, failing for fizzbuzz 30

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,10 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it "returns 'fizz' when passed 3" do
+  it "returns 'fizz' when passed multiples of 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
   end
 end
 
@@ -13,13 +15,15 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
-  it "returns 'fizz' when passed 6" do
-    expect(fizzbuzz(6)).to eq 'fizz'
+  it "returns 'buzz' when passed multiples of 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
 
 describe 'fizzbuzz' do
-  it "returns 'fizz' when passed 9" do
-    expect(fizzbuzz(9)).to eq 'fizz'
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
